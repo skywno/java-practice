358497cfb4cfad77c87acf95a64d527bcd980aff
Gyeha Lim <skywno@gmail.com>
2022-01-19 14:55:41 +0200

    Completed static initialization exercise

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,20 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static {
+        Scanner input = new Scanner(System.in);
+        int breadth = input.nextInt();
+        int height = input.nextInt();
+        if (breadth <= 0 || height <= 0){
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        } else {
+            System.out.println(breadth * height);
+        }
+    }
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner sc = new Scanner(System.in);
-        int lineNum = 1;
-        while (sc.hasNext()){
-            System.out.printf("%d %s%n", lineNum, sc.nextLine());
-            lineNum++;
-        }
+
+
     }
 }
