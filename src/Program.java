ac04967aff9c6060dafdcd33d8a38c0b7976b680
Gyeha Lim <skywno@gmail.com>
2022-01-29 11:33:28 +0200

    Studied how to convert numbers in decimal to hexadecimal.

--- src/Program.java
+++ src/Program.java
@@ -1,35 +1,40 @@
 import java.util.InputMismatchException;
+import java.util.Locale;
 import java.util.function.BiFunction;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Program {
-    public static double mean(int[] nums) {
-        double length = nums.length;
-        int sum = IntStream.of(nums).reduce(0, (int a, int b) -> a+b);
-        return Math.round((sum / length * 100)) / 100.0;
-    }
+    public static String toHex(String  str) {
+        char[] chars = str.toCharArray();
 
-    public static double mean2(int[] nums) {
-        double average = IntStream.of(nums).average().getAsDouble();
-        return Math.round(average * 100) / 100.0;
-    }
-
-    //source: https://edabit.com/challenge/c52kNwPuWo5kp9x4H
-    public static String flipEndChars(String s) {
-        if (s.length() < 2) return "Incompatible.";
-        else if (s.charAt(0) == s.charAt(s.length()-1)) return "Two's a pair.";
-        else return s.charAt(s.length() -1) + s.substring(1,s.length() - 1) + s.charAt(0);
-	}
-
-    //source: https://edabit.com/challenge/gyfsGx7KrGLscxFrD
-    public static int calculator(int num1, char operator, int num2) {
-	    if (operator == '+') return num1 + num2;
-        else if (operator == '*') return num1 * num2;
-        else if (operator == '-') return num1 - num2;
-        else{
-            if (operator == '/' && num2 == 0) return 0;
-            return num1 / num2;
+        StringBuilder builder = new StringBuilder();
+        for (char c : chars) {
+            builder.append(Integer.toHexString(c).toLowerCase(Locale.ROOT)).append(" ");
         }
+        System.out.println(builder.toString().trim());
+        return builder.toString().trim();
 
     }
+    public static String toHex2(String str) {
+        Stream<String> stream = Stream.of(str.split(""));
+        String ret = stream.map( s -> String.format("%h ", s.charAt(0)))
+                            .reduce("", (String a, String b) -> a + b)
+                            .trim();
+                            //.toLowerCase(Locale.ROOT); %H --> %h 로 바꿀 경우 불필요.
+        return ret;
+    }
+    private static int getPercentage(String score){
+        if (score.charAt(score.length()-1) != '%') throw new IllegalArgumentException("the argument doen't end with '%'");
+        return Integer.parseInt(score.substring(0, score.length()-1));
+
+    }
+    public static String gradePercentage(String userScore, String passScore){
+        if (getPercentage(userScore) < getPercentage(passScore))
+            return "You FAILED the Exam";
+        else
+            return "You PASSED the Exam";
+    }
+
 }
+
--- test/ProgramTest.java
+++ test/ProgramTest.java
@@ -6,15 +6,25 @@ import static org.testng.Assert.*;
 public class ProgramTest {
 
     @Test
-    public void testMean() {
-        Assert.assertEquals(Program.mean2(new int[]{1, 0, 4, 5, 2, 4, 1, 2, 3, 3, 3}), 2.55);
+    public void TestConversionStringToHex(){
+        String str = Program.toHex2("hello world");
+        boolean comp = str.equals("68 65 6c 6c 6f 20 77 6f 72 6c 64");
+        Assert.assertTrue(comp);
+        Assert.assertEquals(Program.toHex2("hello world"), "68 65 6c 6c 6f 20 77 6f 72 6c 64");
     }
 
     @Test
-    public void testString() {
-        String result = Program.flipEndChars("Cat, dog, and mouse.");
-        boolean comp = result.equals(".at, dog, and mouseC");
-        Assert.assertTrue(comp);
+    public void TestGrade1(){
+        Assert.assertEquals(Program.gradePercentage("85%", "85%"), "You PASSED the Exam");
     }
 
+    @Test
+    public void TestGrade2(){
+        Assert.assertEquals(Program.gradePercentage("99%", "85%"), "You PASSED the Exam");
+    }
+
+    @Test
+    public void TestGrade3(){
+        Assert.assertEquals(Program.gradePercentage("65%", "85%"), "You FAILED the Exam");
+    }
 }
